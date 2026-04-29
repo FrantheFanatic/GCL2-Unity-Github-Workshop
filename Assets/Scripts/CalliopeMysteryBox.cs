05be90cc702345ce077a7c38bd92b0c452e59f7d
Calsacc <calliopechai@gmail.com>
2026-04-29 11:48:14 +0800

    [Calliope] Added Mystery Box sprite, Mystery box script, and Mystery box

---
+++ Assets/Scripts/CalliopeMysteryBox.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Calliope : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("This is a lucky mystery box");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
