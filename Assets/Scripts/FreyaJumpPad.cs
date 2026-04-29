fa1258f92a43894ec0be47b46a36170bd653a70c
Francis <freya.leong@gmail.com>
2026-04-29 11:16:02 +0800

    [Freya] AddedJump pad sprite, Jump pad script, and new platform

---
+++ Assets/Scripts/FreyaJumpPad.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class FreyaJumpPad : MonoBehaviour
+{
+    private float bounce = 10f;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up*bounce, ForceMode2D.Impulse);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
